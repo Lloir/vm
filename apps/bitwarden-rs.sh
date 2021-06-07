848358452e5bb788113dee137464ce4de80d86bc
THIS IS MY NAME <Lloir@users.noreply.github.com>
2021-06-07 18:25:05 +0100

    Update bitwarden-rs.sh

--- apps/bitwarden-rs.sh
+++ apps/bitwarden-rs.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME="Bitwarden RS"
 SCRIPT_EXPLAINER="Bitwarden RS is an unofficial Bitwarden server API implementation in Rust.
 It has less hardware requirements and therefore runs on nearly any hardware."
 # shellcheck source=lib.sh
-source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+source /var/scripts/fetch_lib.sh || source <(curl -sL https://raw.githubusercontent.com/Lloir/vm/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
